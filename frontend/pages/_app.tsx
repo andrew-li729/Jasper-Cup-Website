777d1cc2e872250ca549028bdbe6be5fef264900
Miles Hilado <86809902+Shad0wgale@users.noreply.github.com>
2024-12-19 21:56:53 -0600

    added background to home

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,12 +1,18 @@
 import type { AppProps } from 'next/app';
 import Navbar from "../components/Navbar";
-import ".//globals.css";
+import "./globals.css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Navbar />
-      <Component {...pageProps} />
+      <div>
+        <h1>Welcome to My Next.js App</h1>
+        <p>This is a basic HTML boilerplate rendered with the Navbar.</p>
+        <footer>
+          <p>Footer content goes here.</p>
+        </footer>
+      </div>
     </>
   );
 };
