5265dc2804b13110cc8005a2d52887b52154a2b8
Miles Hilado <86809902+Shad0wgale@users.noreply.github.com>
2024-12-30 19:23:29 -0600

    added example route for future testing

--- backend/app.js
+++ backend/app.js
@@ -12,3 +12,14 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+app.get('/api/data', async (req, res) => {
+  try {
+    const pool = await poolPromise; // Get the connection pool
+    const result = await pool.request().query('SELECT * FROM races'); // Execute query
+    res.json(result.recordset); // Send the result
+  } catch (err) {
+    console.error('Database query failed: ', err);
+    res.status(500).send('Error querying the database');
+  }
+});
