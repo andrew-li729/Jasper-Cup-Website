4364846509e018b3bb93ecdc165f5dd6881ffe9d
Andrew Li <55461385+andrew-li729@users.noreply.github.com>
2024-12-31 00:25:04 -0600

    added config folder to import in app.js

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,8 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import sql from 'mssql';
-import poolPromise from './db.js';
-import { port } from './config.js';
+import poolPromise from './config/db.js';
+import { port } from './config/config.js';
 const app = express();
 import driverRoutes from './routes/driverRoutes.js';
 
