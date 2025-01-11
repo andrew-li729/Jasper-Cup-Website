e93c0ab4c11a241295c306897d2cac9733c996fe
Miles Hilado <Mileshilado@yahoo.com>
2025-01-11 11:29:35 -0600

    removed deprecated component

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import Head from 'next/head'; // Optional: for managing the <head> element
 import Link from 'next/link'; // Optional: for navigation
 
-import DriversList from '@/components/Driverslist';
 import TwitchChannel from '@/components/Twitch';
 
 
@@ -22,10 +21,7 @@ const IndexPage = () => {
 
 
     <TwitchChannel channelName="JasperCup"></TwitchChannel>
-      
-    <div className="shadow-lg flex justify-center space-x-8 text-white w-full">
-      <DriversList data={driverData} title="Jasper Cup Drivers" />
-    </div>
+    
 
     </>
   );
